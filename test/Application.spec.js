6b65575911f9092637515ca2fcd0e514846132a7
Eric Sayler <easayler@gmail.com>
2017-01-08 11:52:31 -0700

    add basic tests for Application, ExpenseInput, and Input components

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,24 +1,46 @@
 import React from 'react';
-
 import { shallow, mount, render } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 
 import Application from '../lib/components/Application'
 import Button from '../lib/components/Button'
 import DailyExpenses from '../lib/components/DailyExpenses'
 import Input from '../lib/components/Input'
 import MoneyQuotes from '../lib/components/MoneyQuotes'
-import MonthyExpenses from '../lib/components/MonthyExpenses'
+// import MonthyExpenses from '../lib/components/MonthyExpenses'
+import ExpenseInput from '../lib/components/ExpenseInput'
 import MonthTitle from '../lib/components/MonthTitle'
 import UserInput from '../lib/components/UserInput'
 
-import Application from '../lib/components/Application';
-
-describe('Application', () => {
+describe('<Application />', () => {
 
   it('renders as a <div>', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div');
   });
+
+  it('should render one <UserInput /> component', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.find(UserInput)).to.have.length(1);
+  });
+
+  it('should render two <ExpenseInput /> components', () => {
+    const wrapper = shallow(<Application />);
+    expect(wrapper.find(ExpenseInput)).to.have.length(2);
+  });
+
+  it('should render one <DailyExpenses /> component', () => {
+    const wrapper = shallow(<Application />);
+    expect(wrapper.find(DailyExpenses)).to.have.length(1);
+  });
+
+  it.skip('should render one <MonthlyExpenses /> component', () => {
+    const wrapper = shallow(<Application />);
+    expect(wrapper.find(MonthlyExpenses)).to.have.length(1);
+  });
+
+
+
+
 
 });
