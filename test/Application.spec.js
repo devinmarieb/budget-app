2f90be7d75bd05b7972b92ef177fb23052efc096
Devin Beliveau <devinmarieb@gmail.com>
2017-01-08 10:41:22 -0700

    Start styling, start setting up test suite

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -3,6 +3,15 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
 
+import Application from '../lib/components/Application'
+import Button from '../lib/components/Button'
+import DailyExpenses from '../lib/components/DailyExpenses'
+import Input from '../lib/components/Input'
+import MoneyQuotes from '../lib/components/MoneyQuotes'
+import MonthyExpenses from '../lib/components/MonthyExpenses'
+import MonthTitle from '../lib/components/MonthTitle'
+import UserInput from '../lib/components/UserInput'
+
 import Application from '../lib/components/Application';
 
 describe('Application', () => {
